38f4bc7450b8825fa94285208296d2dea2623ed2
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2023-11-08 18:36:57 +0530

    Phase2_Activity3_2

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { HomeComponent } from './home/home.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
   {path:'sign-up', component:SignUpComponent},                     //adding routes
   {path:'login', component:LoginComponent},
-  {path:'', redirectTo:'/home',pathMatch:'full'},
+  {path:'forgot-password', component:ForgotPasswordComponent},
 ];
 
 @NgModule({
